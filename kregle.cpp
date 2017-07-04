d26829cd606142a3e2368acca921e1966069f11c
mjedros <mjedros@gmail.com>
2017-07-04 19:30:26 +0200

    Correct if else statement

--- kregle.cpp
+++ kregle.cpp
@@ -7,33 +7,30 @@
 using namespace std;
 
 unsigned int check(unsigned int xthrow);
-unsigned int take_players ();
-map <int, string> TakeName ();
-void WriteName ();
-unsigned int CheckBonus ();
-void LastBonusThrows ();
+unsigned int take_players();
+map<int, string> TakeName();
+void WriteName();
+unsigned int CheckBonus(size_t bonusIndex);
+void LastBonusThrows();
 
-const unsigned int players=take_players();
+const unsigned int players = take_players();
 
 map<int, string> name;
-  vector<int> points(players, 0);
-  vector<int> bonus(players, 0);
+vector<int> points(players, 0);
+vector<int> bonus(players, 0);
 
-    int p = 0;
-	int i = 0;
-	unsigned int throw1, throw2, spare, strike, oframe;
+int i = 0;
+unsigned int throw1, throw2, spare, strike, oframe;
 
 int main()
 
 {
-
+  size_t p = 0;
   name = TakeName();
   WriteName();
-  
 
   cout << "\n \n PROGRAM ZLICZA PUNKTACJE GRY KREGLE \n";
 
-  
   for (int i = 1; i < 11; i++) {
     cout << "\n RUNDA " << i << endl;
     for (p = 0; p < players; p++) {
@@ -47,15 +44,13 @@ int main()
         cout << "STRIKE zdobyłeś \t" << throw1 << "\t punktów";
 
         points[p] += throw1;
-		  if (i > 1)
-		points[p] = CheckBonus ();
+        if (i > 1) points[p] = CheckBonus(p);
         bonus[p] = 2;
       } else {
         cout << "podaj liczbe wyrzuconych kregli w drugim rzucie \t";
         cin >> throw2;
         throw2 = check(throw2);
-		  if (i > 1)
-		points[p] = CheckBonus ();
+        if (i > 1) points[p] = CheckBonus(p);
 
         if (throw1 + throw2 == 10) {
           spare = throw1 + throw2;
@@ -117,16 +112,15 @@ int main()
 
   //////
 
-  void LastBonusThrows ();
-
+  void LastBonusThrows();
 
   cout << "\n \n \n PODSUMOWANIE FINAL\n\n";
-    vector<int>::iterator see;
-    p = 0;
-    for (see = points.begin(); see != points.end(); ++see) {
-      cout << name[p] << "\t" << *see << endl;
-      p++;
-	}
+  vector<int>::iterator see;
+  p = 0;
+  for (see = points.begin(); see != points.end(); ++see) {
+    cout << name[p] << "\t" << *see << endl;
+    p++;
+  }
 
   cin.get();
   cin.get();
@@ -146,29 +140,26 @@ unsigned int check(unsigned int xthrow)
   return xthrow;
 }
 
-unsigned int take_players ()
-	 {
-		 
-		  cout<< " podaj liczbe graczy - MAX 10 \t";
-	 unsigned int xplayers;
-	 cin>> xplayers;
-	  xplayers = check (xplayers);
-	 while (xplayers == 0)
-		 {
-			 cout<< "\n chyba nie trudno się domyslic, ze 0 nie gra w kregle, podaj liczbe wieksza od 0 i mniejsza od 10 POWODZENIA \n";
-			 cin>>xplayers;
-		}
-	 
-	 xplayers = check (xplayers);
-		 
-	 return xplayers;
-	 }
+unsigned int take_players() {
+  cout << " podaj liczbe graczy - MAX 10 \t";
+  unsigned int xplayers;
+  cin >> xplayers;
+  xplayers = check(xplayers);
+  while (xplayers == 0) {
+    cout << "\n chyba nie trudno się domyslic, ze 0 nie gra w kregle, podaj "
+            "liczbe wieksza od 0 i mniejsza od 10 POWODZENIA \n";
+    cin >> xplayers;
+  }
 
-map <int, string> TakeName ()
-{
-	int p=0;
-	string player;
-	 map<int, string> name;
+  xplayers = check(xplayers);
+
+  return xplayers;
+}
+
+map<int, string> TakeName() {
+  int p = 0;
+  string player;
+  map<int, string> name;
   while (p < players)
 
   {
@@ -178,65 +169,53 @@ map <int, string> TakeName ()
     name[p] = player;
     p++;
   }
-return name;
+  return name;
 }
 
-void WriteName ()
+void WriteName()
 
 {
-	  cout << "\n \n oto lista graczy \n \n";
+  cout << "\n \n oto lista graczy \n \n";
   int p = 0;
-  while (p < players) 
-  {
+  while (p < players) {
     cout << name[p] << "\n";
     p++;
   }
-
 }
 
-unsigned int CheckBonus ()
-{
-	       // naliczanie premii 
-        {
-          if (bonus[p] == 2)
-            if (throw1 == 10)
-              points[p] += throw1 ;
-            else
-              points[p] += throw1 + throw2;
-          else if (bonus[p] == 1)
-            points[p] += throw1;
-        }  // koniec naliczania 
-		   return points[p];
+unsigned int CheckBonus(size_t bonusIndex) {
+  // naliczanie premii -- TODO comments also in english
+
+  if (bonus[bonusIndex] == 2) {
+    if (throw1 == 10)
+      points[bonusIndex] += throw1;
+    else
+      points[bonusIndex] += throw1 + throw2;
+  } else if (bonus[bonusIndex] == 1)
+    points[bonusIndex] += throw1;
+  // koniec naliczania
+  return points[bonusIndex];
 }
 
-void LastBonusThrows ()
-{
-	for (p=0; p<players; p++)
-	{
-	if (bonus[p]==2)
-		{
-			cout<< "\n Masz dodatkowe dwa rzuty \n podaj ile wyrzuciles w pierwszym rzucie: \t";
-			cin>> throw1;
-			if (throw1==10)
-			{
-				cout<< "\n STRIKE";
-				points[p]+= throw1*2;
-			}
-			else
-			{
-				cout<< "\n podaj ile wyrzuciłeś w drugim rzucie: \t";
-					cin>> throw2;
-				points[p]+=throw1+throw2;
-			}
-		}
-	else
-		if (bonus[p]==1)
-		{
-			cout<< "\n Masz dodatkowy rzut \n podaj ile wyrzuciles w pierwszym rzucie: \t";
-			cin>> throw1;
-			points[p]+=throw1;
-
-		}
-
-	}
+void LastBonusThrows() {
+  for (size_t p = 0; p < players; p++) {
+    if (bonus[p] == 2) {
+      cout << "\n Masz dodatkowe dwa rzuty \n podaj ile wyrzuciles w pierwszym "
+              "rzucie: \t";
+      cin >> throw1;
+      if (throw1 == 10) {
+        cout << "\n STRIKE";
+        points[p] += throw1 * 2;
+      } else {
+        cout << "\n podaj ile wyrzuciłeś w drugim rzucie: \t";
+        cin >> throw2;
+        points[p] += throw1 + throw2;
+      }
+    } else if (bonus[p] == 1) {
+      cout << "\n Masz dodatkowy rzut \n podaj ile wyrzuciles w pierwszym "
+              "rzucie: \t";
+      cin >> throw1;
+      points[p] += throw1;
+    }
+  }
 }
