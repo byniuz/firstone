6e514fa23ce42e5cba7b72a93482c162b6fa1463
byniuz <zstawarz@gmail.com>
2017-07-11 22:21:54 +0200

    Zeby nie było - jest Classa, kiepska jeszcze ale sie robi

--- kregle.cpp
+++ kregle.cpp
@@ -5,64 +5,117 @@
 #include <vector>
 using namespace std;
 
-unsigned int take_players(unsigned int xplayers);
-map<int, string> TakeName();
-void WriteName();
+class PlayerName
+{
+public:
+
+	bool CheckNumber(unsigned int TestNumber) {
+
+		bool check = (TestNumber <= 10);
+
+		return check;
+	}
+	unsigned int GetOneNumberAndCheckIt() {
+		unsigned int xthrow;
+		cin >> xthrow;
+		while (!CheckNumber(xthrow))
+		{
+			cout << "nie umiesz BUCU policzyć do 10 \n sprobuj policzyć jeszcze raz "
+				"wykorzystujac reszte mozgu \n";
+			cin >> xthrow;
+		}
+		return xthrow;
+	}
+
+	const unsigned int players = take_players();
+
+	unsigned int take_players() {
+		cout << " podaj liczbe graczy - MAX 10 \t";
+
+		unsigned int xplayers;
+		xplayers = GetOneNumberAndCheckIt();
+		while (xplayers == 0) {
+			cout << "\n chyba nie trudno się domyslic, ze 0 nie gra w kregle, podaj "
+				"liczbe wieksza od 0 i mniejsza od 10 POWODZENIA \n";
+			xplayers = GetOneNumberAndCheckIt();
+		}
+
+
+		return xplayers;
+	}
+
+	map<int, string> name;
+	map<int, string> TakeName(unsigned int p) {
+		
+		string player;
+		while (p < players)
+
+		{
+			p++;
+			cout << "podaj imię gracza numer " << p << "\t";
+			p--;
+			cin >> player;
+			name[p] = player;
+			p++;
+		}
+		return name;
+	}
+
+	void WriteName()
+
+	{
+		cout << "\n \n oto lista graczy \n \n";
+		unsigned int p = 0;
+			while (p < players) {
+			cout << name[p] << "\n";
+			p++;
+		}
+	}
+
+
+};
+
+
+
 unsigned int CheckBonus(size_t bonusIndex);
-void LastBonusThrows();
+void LastBonusThrows(PlayerName names);
 
-const unsigned int players = take_players(players);
 
-map<int, string> name;
-vector<int> points(players, 0);
-vector<int> bonus(players, 0);
+
+vector<int> points(10, 0);
+vector<int> bonus(10, 0);
 
 unsigned int throw1, throw2, spare, strike, oframe;
 
-bool CheckNumber(unsigned int TestNumber) {
-
-	bool check = (TestNumber <= 10);
-
-	return check;
-}
-unsigned int GetOneThrowPoints() {
-	unsigned int xthrow;
-	cin >> xthrow;
-	while (!CheckNumber(xthrow))
-	{
-		cout << "nie umiesz BUCU policzyć do 10 \n sprobuj policzyć jeszcze raz "
-			"wykorzystujac reszte mozgu \n";
-		cin >> xthrow;
-	}
-	return xthrow;
-}
 
 
-void Summary() {
+
+void Summary(PlayerName names) {
 	
 	size_t p = 0;
-	for (p=0; p<players; p++) {
-		cout << name[p] << "\t" << points[p]<< endl;
+	for (p=0; p<names.players; p++) {
+		cout << names.name[p] << "\t" << points[p]<< endl;
 			}
 }
 
 int main()
 {
+	PlayerName names;
 	constexpr uint8_t ROUND_NUMBERS = 10;
 	size_t p = 0;
-	name = TakeName();
-	WriteName();
+	names.TakeName(p);
+	names.WriteName();
 
 	cout << "\n \n PROGRAM ZLICZA PUNKTACJE GRY KREGLE \n";
 
 	for (int i = 1; i <= ROUND_NUMBERS; i++) {
 		cout << "\n RUNDA " << i << endl;
-		for (p = 0; p < players; p++) {
-			cout << "\n \n RZUCA \t \t" << name[p];
+		for (p = 0; p < names.players; p++) {
+			cout << "\n \n RZUCA \t \t" << names.name[p];
 		begin:
 			cout << "\n \n podaj liczbę wyrzuconych kręgli \t";
 			
-			throw1 = GetOneThrowPoints();
+			throw1 = names.GetOneNumberAndCheckIt();
 
 			if (throw1 == 10) {
 				cout << "STRIKE zdobyłeś \t" << throw1 << "\t punktów";
@@ -74,7 +127,7 @@ int main()
 			else {
 				cout << "podaj liczbe wyrzuconych kregli w drugim rzucie \t";
 				
-				throw2 = GetOneThrowPoints();
+				throw2 = names.GetOneNumberAndCheckIt();
 				if (i > 1) points[p] = CheckBonus(p);
 
 				if (throw1 + throw2 == 10) {
@@ -100,12 +153,12 @@ int main()
 		}
 
 		cout << "\n \n \n PODSUMOWANIE \n\n";
-		Summary();
+		Summary(names);
 	}
-	LastBonusThrows();
+	LastBonusThrows(names);
 
 	cout << "\n \n \n PODSUMOWANIE FINAL\n\n";
-	Summary();
+	Summary(names);
 
 	cin.get();
 	cin.get();
@@ -114,47 +167,8 @@ int main()
 }
 
 
-unsigned int take_players(unsigned int xplayers) {
-	cout << " podaj liczbe graczy - MAX 10 \t";
-	
-	
-	xplayers = GetOneThrowPoints();
-	while (xplayers == 0) {
-		cout << "\n chyba nie trudno się domyslic, ze 0 nie gra w kregle, podaj "
-			"liczbe wieksza od 0 i mniejsza od 10 POWODZENIA \n";
-		xplayers = GetOneThrowPoints();
-	}
 
-	
-	return xplayers;
-}
 
-map<int, string> TakeName() {
-	int p = 0;
-	string player;
-	map<int, string> name;
-	while (p < players)
-
-	{
-		cout << "podaj imię gracza numer " << p << "\t";
-
-		cin >> player;
-		name[p] = player;
-		p++;
-	}
-	return name;
-}
-
-void WriteName()
-
-{
-	cout << "\n \n oto lista graczy \n \n";
-	int p = 0;
-	while (p < players) {
-		cout << name[p] << "\n";
-		p++;
-	}
-}
 
 unsigned int CheckBonus(size_t bonusIndex) {
 	//  bonus counting - start
@@ -171,26 +185,26 @@ unsigned int CheckBonus(size_t bonusIndex) {
 	return points[bonusIndex];
 }
 
-void LastBonusThrows() {
-	for (size_t p = 0; p < players; p++) {
+void LastBonusThrows(PlayerName names) {
+	for (size_t p = 0; p < names.players; p++) {
 		if (bonus[p] == 2) {
-			cout << "\n"<<name[p]<< "\t Masz dodatkowe dwa rzuty \n podaj ile wyrzuciles w pierwszym "
+			cout << "\n"<<names.name[p]<< "\t Masz dodatkowe dwa rzuty \n podaj ile wyrzuciles w pierwszym "
 				"rzucie: \t";
-			throw1 = GetOneThrowPoints();
+			throw1 = names.GetOneNumberAndCheckIt();
 			if (throw1 == 10) {
 				cout << "\n STRIKE";
 				points[p] += throw1 * 2;
 			}
 			else {
 				cout << "\n podaj ile wyrzuciłeś w drugim rzucie: \t";
-				throw2 = GetOneThrowPoints();
+				throw2 = names.GetOneNumberAndCheckIt();
 				points[p] += throw1 + throw2;
 			}
 		}
 		else if (bonus[p] == 1) {
-			cout << "\n"<<name[p]<< "\t Masz dodatkowy rzut \n podaj ile wyrzuciles w pierwszym "
+			cout << "\n"<<names.name[p]<< "\t Masz dodatkowy rzut \n podaj ile wyrzuciles w pierwszym "
 				"rzucie: \t";
-			throw1 = GetOneThrowPoints();
+			throw1 = names.GetOneNumberAndCheckIt();
 			points[p] += throw1;
 		}
 	}
